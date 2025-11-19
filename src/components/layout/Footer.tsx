942c8867ae3849636ef947a06a56ea9013bc0112
rdnormal <mbc9712@hotmail.com>
2025-11-19 23:07:06 +0000

    updated the footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,9 +1,5 @@
 export default function Footer() {
   return (
-    <footer className="footer">
-      <small>
-        <p>&copy; Copyright by <a target="_blank" href="https://github.com/rdnormal">rdnormal</a></p>
-      </small>
-    </footer>
+    <footer className="footer" />
   )
 }
